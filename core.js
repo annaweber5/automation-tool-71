fa2c2aba096c5a5804f88d2713b46876e9bc9780
Nathalie Rousseau <annaweber5443@gmail.com>
2026-08-14 03:44:21 +0200

    performance optimization in core module

--- core.js
+++ core.js
@@ -1,32 +1,36 @@
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+class RobloxAutomation {
+    constructor() {
+        this.actions = [];
+    }
+    addAction(action) {
+        this.actions.push(action);
+    }
+    executeActions() {
+        for (const action of this.actions) {
+            this._performAction(action);
+        }
+    }
+    _performAction(action) {
+        switch (action.type) {
+            case 'move':
+                this._move(action.target);
+                break;
+            case 'click':
+                this._click(action.target);
+                break;
+            default:
+                console.warn('Unknown action type:', action.type);
+        }
+    }
+    _move(target) {
+        // Implement movement logic here
+    }
+    _click(target) {
+        // Implement click logic here
+    }
+}
 
-const fetchJson = async (url) => {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error('Network response was not ok');
-    return await response.json();
-};
-
-const objectToArray = (obj) => Object.keys(obj).map(key => ({ key, value: obj[key] }));
-
-const arrayToObject = (array, keyField) => {
-    return array.reduce((acc, item) => {
-        acc[item[keyField]] = item;
-        return acc;
-    }, {});
-};
-
-const isEmptyObject = (obj) => Object.keys(obj).length === 0;
-
-const mergeObjects = (target, source) => {
-    return { ...target, ...source };
-};
-
-const debounce = (func, delay) => {
-    let timeout;
-    return (...args) => {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), delay);
-    };
-};
-
-export { delay, fetchJson, objectToArray, arrayToObject, isEmptyObject, mergeObjects, debounce };
+const automation = new RobloxAutomation();
+automation.addAction({ type: 'move', target: 'someLocation' });
+automation.addAction({ type: 'click', target: 'someButton' });
+automation.executeActions();
